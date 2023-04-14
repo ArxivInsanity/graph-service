2901d6388794c54fb69d25f1567cac4cc8780a86
anushkumarv <anush070@gmail.com>
2023-04-13 18:27:50 -0600

    AR-23 added isSeed paper handler

--- src/services/graphSearchService.go
+++ src/services/graphSearchService.go
@@ -1,27 +1,33 @@
 package services
 
 import (
+	"log"
+	"net/http"
+
 	. "github.com/ArxivInsanity/graph-service/src/util"
 	"github.com/gin-gonic/gin"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
-	"log"
-	"net/http"
 )
 
-func IsSeedPaper() gin.HandlerFunc {
+func IsSeedPaperHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		dbContext, dbSession := GetDBConnectionFromContext(ctx)
 		paperId := ctx.Param("paperId")
-		log.Printf("URL Params: %v", paperId)
-		isSeedCypher := "RETURN EXISTS( (:SEED_PAPER)-[:SEED]-(:PAPER {title: $title})) as isSeed"
-		cypherParam := map[string]any{
-			"title": paperId,
-		}
-		result, err := dbSession.Run(dbContext, isSeedCypher, cypherParam)
-		record, err := neo4j.CollectWithContext(dbContext, result, err)
-		isSeed, _ := record[0].Get("isSeed")
-		log.Printf("Paper: %s is Seed Paper: %v", paperId, isSeed)
-		PanicOnErr(err)
+		log.Printf("Seed Paper handler URL Params: %v", paperId)
+		isSeed := IsSeedPaper(paperId, ctx)
 		ctx.JSON(http.StatusOK, isSeed)
 	}
+}
+
+func IsSeedPaper(paperId string, ctx *gin.Context) bool {
+	dbContext, dbSession := GetDBConnectionFromContext(ctx)
+	isSeedCypher := "RETURN EXISTS( (:SEED_PAPER)-[:SEED]-(:PAPER {title: $title})) as isSeed"
+	cypherParam := map[string]any{
+		"paperId": paperId,
+	}
+	result, err := dbSession.Run(dbContext, isSeedCypher, cypherParam)
+	record, err := neo4j.CollectWithContext(dbContext, result, err)
+	isSeed, _ := record[0].Get("isSeed")
+	log.Printf("Paper: %s is Seed Paper: %v", paperId, isSeed)
+	PanicOnErr(err)
+	return isSeed.(bool)
 }
