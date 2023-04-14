fb4da7beff171aed7dda577522ffe22daaaed49f
anushkumarv <anush070@gmail.com>
2023-04-14 01:30:12 -0600

    AR-15 BFS logic for search service messy code

--- src/services/graphSearchService.go
+++ src/services/graphSearchService.go
@@ -14,8 +14,18 @@ func IsSeedPaperHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		paperId := ctx.Param("paperId")
 		log.Printf("Seed Paper handler URL Params: %v", paperId)
-		isSeed := getNode(paperId, ctx)
+		isSeed := IsSeedPaper(paperId, ctx)
 		ctx.JSON(http.StatusOK, isSeed)
+	}
+}
+
+func GraphHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		paperId := ctx.Param("paperId")
+		log.Printf("graph Paper handler URL Params: %v", paperId)
+		seedNode := GetNode(paperId, ctx)
+		visited := bfs(seedNode, 3, true, ctx)
+		ctx.JSON(http.StatusOK, visited)
 	}
 }
 
@@ -36,14 +46,13 @@ func IsSeedPaper(paperId string, ctx *gin.Context) bool {
 
 type Node struct {
 	paperId       string
-	citationCount int
-	year          int
+	citationCount int64
+	year          int64
 	title         string
-	citation      []*Node
 	reference     []*Node
 }
 
-func getNode(rootPaperId string, ctx *gin.Context) *Node {
+func GetNode(rootPaperId string, ctx *gin.Context) *Node {
 	dbContext, dbSession := GetDBConnectionFromContext(ctx)
 	cypher := "MATCH (p:PAPER {paperId: $rootPaperId}) RETURN p"
 	// referenceCypher := "MATCH (p:PAPER {paperId: $rootPaperId})-[r:REFERENCE]-(n:PAPER) RETURN p, n"
@@ -52,38 +61,72 @@ func getNode(rootPaperId string, ctx *gin.Context) *Node {
 	}
 	result, err := dbSession.Run(dbContext, cypher, cypherParam)
 	PanicOnErr(err)
-	citationRecord, err := neo4j.CollectWithContext(dbContext, result, err)
+	nodeRecord, err := neo4j.CollectWithContext(dbContext, result, err)
 	PanicOnErr(err)
-	citationNeo4jRecord, _ := citationRecord[0].Get("p")
-	citationNeo4jProps := citationNeo4jRecord.(dbtype.Node).Props
+	nodeNeo4jRecord, _ := nodeRecord[0].Get("p")
+	nodeNeo4jProps := nodeNeo4jRecord.(dbtype.Node).Props
 	n := Node{paperId: rootPaperId,
-		citationCount: citationNeo4jProps["citationCount"].(int),
-		year:          citationNeo4jProps["year"].(int),
-		title:         citationNeo4jProps["title"].(string)}
+		citationCount: nodeNeo4jProps["citationCount"].(int64),
+		year:          nodeNeo4jProps["year"].(int64),
+		title:         nodeNeo4jProps["title"].(string)}
 	return &n
 }
 
-// func bfs(n *Node, depth int, ctx *gin.Context) {
-// 	queue := []*Node{n}
-// 	visited := map[string]Node{}
+func bfs(n *Node, depth int, citation bool, ctx *gin.Context) map[string]Node {
+	dbContext, dbSession := GetDBConnectionFromContext(ctx)
+	queue := []*Node{n}
+	visited := map[string]Node{}
 
-// 	for len(queue) > 0 && depth > 0 {
-// 		level_size := len(queue)
-// 		cypher := "MATCH (p:PAPER {paperId: $paperId}) - [r:CITATION] -> (n:PAPER) RETURN n"
-// 		cypherParam := map[string]any{
-// 			"rootPaperId": rootPaperId,
-// 		}
-// 		for i := 0; i < level_size; i++ {
-// 			current := queue[0]
-// 			queue = queue[1:]
-// 			visited[current.name] = *current
-// 			for _, nghr := range n.neighbours {
-// 				if _, exists := visited[nghr.name]; !exists {
-// 					queue = append(queue, nghr)
-// 				}
-// 			}
-// 		}
-// 		depth -= 1
-// 	}
+	for len(queue) > 0 && depth > 0 {
+		level_size := len(queue)
+		for i := 0; i < level_size; i++ {
+			current := queue[0]
+			queue = queue[1:]
+			cypher := "MATCH (p:PAPER {paperId: $paperId}) - [r:REFERENCES] -> (n:PAPER) RETURN n"
+			cypherParam := map[string]any{
+				"paperId": current.paperId,
+			}
+			result, err := dbSession.Run(dbContext, cypher, cypherParam)
+			PanicOnErr(err)
+			linkRecord, err := neo4j.CollectWithContext(dbContext, result, err)
+			PanicOnErr(err)
+			for i := 0; i < len(linkRecord); i++ {
+				linkNeo4jRecord, _ := linkRecord[i].Get("n")
+				linkNeo4jProps := linkNeo4jRecord.(dbtype.Node).Props
+				child := Node{paperId: linkNeo4jProps["paperId"].(string),
+					citationCount: linkNeo4jProps["citationCount"].(int64),
+					year:          linkNeo4jProps["year"].(int64),
+					title:         linkNeo4jProps["title"].(string)}
+				current.reference = append(current.reference, &child)
+			}
+			for _, child := range current.reference {
+				if _, exists := visited[child.paperId]; !exists {
+					queue = append(queue, child)
+				}
+			}
+			visited[current.paperId] = *current
+			cypher = "MATCH (p:PAPER {paperId: $paperId}) <- [r:REFERENCES] - (n:PAPER) RETURN n"
+			cypherParam = map[string]any{
+				"paperId": current.paperId,
+			}
+			result, err = dbSession.Run(dbContext, cypher, cypherParam)
+			PanicOnErr(err)
+			linkRecord, err = neo4j.CollectWithContext(dbContext, result, err)
+			PanicOnErr(err)
+			for i := 0; i < len(linkRecord); i++ {
+				linkNeo4jRecord, _ := linkRecord[i].Get("n")
+				linkNeo4jProps := linkNeo4jRecord.(dbtype.Node).Props
+				parent := Node{paperId: linkNeo4jProps["paperId"].(string),
+					citationCount: linkNeo4jProps["citationCount"].(int64),
+					year:          linkNeo4jProps["year"].(int64),
+					title:         linkNeo4jProps["title"].(string)}
+				if _, exists := visited[parent.paperId]; !exists {
+					queue = append(queue, &parent)
+				}
+			}
+		}
 
-// }
+		depth -= 1
+	}
+	return visited
+}
