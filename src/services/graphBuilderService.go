d979dba5d9658df197df766f92e80fa259d9db08
anushkumarv <anush070@gmail.com>
2023-04-15 00:20:14 -0600

    AR-12 BFS logic for graph builder

--- src/services/graphBuilderService.go
+++ src/services/graphBuilderService.go
@@ -2,10 +2,9 @@ package services
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"sort"
 
 	. "github.com/ArxivInsanity/graph-service/src/util"
 	"github.com/gin-gonic/gin"
@@ -20,23 +19,97 @@ func BuildGraphHndler() gin.HandlerFunc {
 }
 
 func BuildGraph(seedPaperId string, ctx *gin.Context) {
-	node := GetPaperDetails((seedPaperId))
+	node := BfsBuilder(seedPaperId, ctx)
 	log.Printf("Build graph Paper details : %v", node)
 	ctx.IndentedJSON(http.StatusOK, node)
 }
 
-func GetPaperDetails(paperId string) Node {
+func GetPaperNode(paperId string) Node {
 	url := viper.Get("s2ag.urlRoot").(string) + paperId + viper.Get("s2ag.paperUrlFields").(string)
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Add("x-api-key", os.Getenv("S2AG_KEY"))
-	resp, err := client.Do(req)
-	PanicOnErr(err)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	PanicOnErr(err)
 	var node Node
-	err = json.Unmarshal(body, &node)
+	err := json.Unmarshal(FetchFromS2ag(url), &node)
 	PanicOnErr(err)
 	return node
+}
+
+func GetReferences(paperId string, reference bool, breadth int) []NodeRefences {
+	url := viper.GetString("s2ag.urlRoot") + paperId
+	if reference {
+		url += viper.GetString("s2ag.referenceUrlFields")
+	} else {
+		url += viper.GetString("s2ag.citationUrlFields")
+	}
+	url += viper.GetString("s2ag.buffer")
+	var resp ResponseReferences
+	err := json.Unmarshal(FetchFromS2ag(url), &resp)
+	PanicOnErr(err)
+	// return []NodeRefences{}
+	if len(resp.Data) <= breadth {
+		return resp.Data
+	} else {
+		sort.Slice(resp.Data, func(i, j int) bool {
+			if resp.Data[i].IsInfluential {
+				return true
+			} else {
+				return false
+			}
+		})
+		return resp.Data[:breadth]
+	}
+}
+
+func GetNodeReferences(nodeReferences []NodeRefences, reference bool) []Node {
+	var nodes []Node
+	for _, child := range nodeReferences {
+		paperId := ""
+		if reference {
+			paperId = child.CitedPaper.RefPaperId
+		} else {
+			paperId = child.CitingPaper.CitPaperId
+		}
+		nodes = append(nodes, GetPaperNode(paperId))
+	}
+	return nodes
+}
+
+func BfsBuilder(seedPaperId string, ctx *gin.Context) map[string]Node {
+	depth := viper.GetInt("graph.depth")
+	breadth := viper.GetInt("graph.refBreadth")
+	var nodeRef []NodeRefences
+	var nodes []Node
+	n := GetPaperNode(seedPaperId)
+	queue := []Node{n}
+	visited := map[string]Node{}
+
+	for len(queue) > 0 && depth > 0 {
+		level_size := len(queue)
+		for i := 0; i < level_size; i++ {
+			current := queue[0]
+			queue = queue[1:]
+			nodeRef = GetReferences(current.PaperId, true, breadth)
+			nodes = GetNodeReferences(nodeRef, true)
+			for _, child := range nodes {
+				current.Reference = append(current.Reference, child)
+			}
+			visited[current.PaperId] = current
+			for _, child := range nodes {
+				if _, exists := visited[child.PaperId]; !exists {
+					queue = append(queue, child)
+				}
+			}
+			nodeRef = GetReferences(current.PaperId, false, breadth)
+			nodes = GetNodeReferences(nodeRef, false)
+			for _, child := range nodes {
+				if _, exists := visited[child.PaperId]; !exists {
+					queue = append(queue, child)
+				}
+			}
+		}
+		depth -= 1
+		// if breadth > 1 {
+		// 	breadth -= 1
+		// }
+
+	}
+	return visited
 }
--- src/util/dataObjects.go
+++ src/util/dataObjects.go
@@ -1,5 +1,19 @@
 package util
 
+type ResponseReferences struct {
+	Data []NodeRefences `json:"data"`
+}
+
+type NodeRefences struct {
+	IsInfluential bool `json:"isInfluential"`
+	CitedPaper    struct {
+		RefPaperId string `json:"paperId"`
+	} `json:"citedPaper"`
+	CitingPaper struct {
+		CitPaperId string `json:"paperId"`
+	} `json:"citingPaper"`
+}
+
 type Author struct {
 	AuthorId string `json:"authorId"`
 	Name     string `json:"name"`
